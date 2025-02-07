1317ac24daaa7062b8842f496a03a7c86881d33e
divyamgit911 <divyam1071.be21@chitkarauniversity.edu.in>
2025-02-07 19:59:39 +0530

    new file:   UI_HTML_CSS_TASK_TECHMAHINDRA 	new file:   lec-3-070225/index.html 	new file:   lec-3-070225/index.js

---
+++ lec-3-070225/index.js
@@ -0,0 +1,30 @@
+// // cannot re-declare block-scope variables
+// // declare all variables at top 
+
+// //loops
+
+// // forEach loop
+// arr = [1,2,3,4,5,6]
+
+// arr.forEach(function (ele) {
+//     console.log(ele);
+// })
+
+// // for in
+// for (let i in arr) {
+//     console.log(`index: ${i} element: ${arr[i]}`); 
+// }
+
+// // for of
+// for (i of arr) {
+//     console.log(i)
+// }
+
+// // while
+
+// // do while
+// do {
+//     console.log(i)
+// } while(i <= 10)
+
+
